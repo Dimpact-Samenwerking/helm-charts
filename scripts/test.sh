b625146360e7da121d82c1caf63f5d77caeba9bc
Caner Ortlek <caner.ortlek@dimpact.nl>
2024-12-04 15:41:07 +0100

    Deleted test script

--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-OPENFORMS_IMAGE_TAG_LATEST=2.7.9
-
-curl -s https://raw.githubusercontent.com/open-formulieren/open-forms/refs/heads/master/CHANGELOG.rst | awk "/^${OPENFORMS_IMAGE_TAG_LATEST}/ {flag=1} /^[0-9]+\\.[0-9]+\\.[0-9]+/  && !/^${OPENFORMS_IMAGE_TAG_LATEST}/ {flag=0} flag"
